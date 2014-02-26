4f32f4583bbacc73e77a32044faa7ec26383b7c1
Harris Lapiroff <harris@chromamine.com>
2014-02-26 18:29:12 -0500

    Added a default AppConfig for clickclick.

--- src/django-clickclick/clickclick/__init__.py
+++ src/django-clickclick/clickclick/__init__.py
@@ -1,1 +1,3 @@
 VERSION = (0, 4, 1)
+
+default_app_config = 'clickclick.apps.ClickClickConfig'
---
+++ src/django-clickclick/clickclick/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+class ClickClickConfig(AppConfig):
+	name = "clickclick"
+	verbose_name = "Click Click"
