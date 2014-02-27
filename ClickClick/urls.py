9a0fbebb0064f782963a7019ce85c2813ab8f447
Harris Lapiroff <harris@chromamine.com>
2014-02-26 19:16:01 -0500

    Restructured to make the base of the repo a python package.

--- ClickClick/urls.py
+++ ClickClick/urls.py
@@ -1,11 +1,19 @@
-from django.conf import settings
 from django.conf.urls import patterns, include, url
-from django.conf.urls.static import static
-from django.contrib import admin
+
+from clickclick.utils import reverse_lazy
 
 urlpatterns = patterns('',
-	url(r'^admin/', include(admin.site.urls)),
-	url(r'^grappelli/', include('grappelli.urls')),
-	url(r'^daguerre/', include('daguerre.urls')),
-	url(r'^', include('clickclick.urls')),
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+	url(r'^$', 'clickclick.views.home', name="clickclick.home"),
+	# photo/set creation and updating
+	url(r'^create/$', 'clickclick.views.create_photoset', name='clickclick.create_photoset'),
+	url(r'^delete/(?P<photoset_slug>[\w-]+)/$', 'clickclick.views.delete_photoset', name='clickclick.delete_photoset'),
+	url(r'^edit/(?P<photoset_slug>[\w-]+)/$', 'clickclick.views.edit_photoset', name='clickclick.edit_photoset'),
+	url(r'^(?P<photoset_slug>[\w-]+)/create/$', 'clickclick.views.add_photo', name='clickclick.add_photo'),
+	url(r'^(?P<photoset_slug>[\w-]+)/delete/(?P<photo_slug>[-\w]+)/$', 'clickclick.views.delete_photo', name='clickclick.delete_photo'),
+	url(r'^(?P<photoset_slug>[\w-]+)/edit/(?P<photo_slug>[-\w]+)/$', 'clickclick.views.edit_photo', name='clickclick.edit_photo'),
+	# photo/set management
+	url(r'^photosets/$', 'clickclick.views.photoset_list', name='clickclick.photoset_list'),
+	# photo/set browsing
+	url(r'^(?P<photoset_slug>[\w-]+)/$', 'clickclick.views.photoset_detail', name='clickclick.photoset'),
+	url(r'^(?P<photoset_slug>[\w-]+)/(?P<photo_slug>[-\w]+)/$', 'clickclick.views.photo_detail', name='clickclick.photo'),
+)
--- src/django-clickclick/clickclick/__init__.py
+++
@@ -1,3 +0,0 @@
-VERSION = (0, 4, 1)
-
-default_app_config = 'clickclick.apps.ClickClickConfig'
