37dfb8e0f8e939a5e34bb999ac352fb7be4c1023
Harris Lapiroff <harris@chromamine.com>
2014-02-28 00:03:46 -0500

    Integrated a Compass project.

---
+++ clickclick/compass/config.rb
@@ -0,0 +1,9 @@
+http_path = "/"
+css_dir = "../static/clickclick/stylesheets"
+sass_dir = "sass"
+images_dir = "../static/clickclick/images"
+javascripts_dir = "../static/clickclick/javascripts"
+output_style = :compressed
+relative_assets = true
+line_comments = false
+preferred_syntax = :sass
