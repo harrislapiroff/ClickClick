c87586c00f6c4830bac27be5bda9856e3b1ed121
Harris Lapiroff <harris@chromamine.com>
2014-03-03 13:15:26 -0500

    Added django-contrib-comments to requirements and settings.

--- example_project/ClickClick/settings.py
+++ example_project/ClickClick/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'django.contrib.humanize',
     'clickclick',
     'daguerre',
+    'django_comments',
 )
 
 MIDDLEWARE_CLASSES = (
